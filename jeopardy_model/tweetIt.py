339e906db688d166135256f165fe5d8f99eb5340
perlm <jason.perlmutter@gmail.com>
2017-03-22 19:13:11 -0400

    tweet text

--- jeopardy_model/tweetIt.py
+++ jeopardy_model/tweetIt.py
@@ -9,7 +9,9 @@ import pandas as pd
 import ConfigParser, os, datetime
 
 def tweetProb(features):
-	tweet = '{n} is a {day}-day champ with ${dol} winnings: #Jeopardy model predicts {p}% prob to win! (as of {date}) https://hastydata.wordpress.com/2016/11/26/modeling-jeopardy-revisited/'.format(n=str(features['name']), day=str(features['days']),dol=str(features['dollars']),p=int(100*round(float(features['prob']),2)),date=str(features['date']) )
+	# getting rid of date, since I'm being stricter about when tweets are sent
+	#tweet = '{n} is a {day}-day champ with ${dol} winnings: #Jeopardy model predicts {p}% prob to win! (as of {date}) https://hastydata.wordpress.com/2016/11/26/modeling-jeopardy-revisited/'.format(n=str(features['name']), day=str(features['days']),dol=str(features['dollars']),p=int(100*round(float(features['prob']),2)),date=str(features['date']) )
+	tweet = '{n} is a {day}-day champ with ${dol} winnings: #Jeopardy model predicts {p}% prob to win! https://hastydata.wordpress.com/2016/11/26/modeling-jeopardy-revisited/'.format(n=str(features['name']), day=str(features['days']),dol=str(features['dollars']),p=int(100*round(float(features['prob']),2)))
 
 	config = ConfigParser.ConfigParser()
 	config.read('{0}/.python_keys.conf'.format(os.path.expanduser("~")))
